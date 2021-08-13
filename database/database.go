825ee0827343cc3122fd8076e98652c8de6fdc0b
Neevash Ramdial <mail@neevash.dev>
2021-08-12 22:05:46 -0400

    create database package

---
+++ database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	DBConn *gorm.DB
+)
+
+func InitDatabase() *gorm.DB {
+	DBConn, err := gorm.Open(sqlite.Open("books.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Unable to connect to database")
+	}
+
+	fmt.Println("Database connection open 🚀")
+	return DBConn
+}
