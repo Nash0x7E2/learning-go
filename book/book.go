ab074be5f906e94f233739bc747076d75e2e0957
Neevash Ramdial <mail@neevash.dev>
2021-08-12 22:55:55 -0400

    implement database calls in book.go

--- book/book.go
+++ book/book.go
@@ -1,26 +1,62 @@
 package book
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/gofiber/fiber"
+	"gorm.io/gorm"
+	"learning_go/database"
+)
 
 type Book struct {
-	name   string `json:"name"`
-	author string `json:"author"`
+	gorm.Model
+	Name   string `json:"name"`
+	Author string `json:"author"`
 	ISBN   string `json:"isbn"`
 }
 
 func GetBooks(c *fiber.Ctx) {
-	c.Send("Hello books")
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+	c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) {
 	id := c.Params("id")
-	c.Send("Hello " + id)
+	db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+
+	if book.Name == "" {
+		c.Status(404).Send("No book found with the ID" + id)
+		return
+	}
+
+	c.JSON(book)
 }
 
 func AddBook(c *fiber.Ctx) {
-	c.Send("Hello add books")
+	db := database.DBConn
+	book := new(Book)
+
+	if err := c.BodyParser(book); err != nil {
+		c.Status(500).Send("Unable to parse JSON data. Please review your request body")
+		return
+	}
+
+	db.Create(&book)
+	c.Status(200).JSON(&book)
 }
 
 func DeleteBook(c *fiber.Ctx) {
-	c.Send("Bye books")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.First(&book, id)
+
+	if book.Name == "" {
+		c.Status(404).Send("No book found with the ID " + id)
+		return
+	}
+	db.Delete(&book)
+	c.SendString("Book removed")
 }
